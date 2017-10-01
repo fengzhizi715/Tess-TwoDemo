9207fff7a2108f08b3907100580d4eaa21c7d62e
fengzhizi715 <fengzhizi715@126.com>
2017-10-02 00:22:24 +0800

    add trained data

--- app/src/main/java/com/cv4j/ocrdemo/MainActivity.java
+++ app/src/main/java/com/cv4j/ocrdemo/MainActivity.java
@@ -1,13 +1,232 @@
 package com.cv4j.ocrdemo;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.cv4j.core.binary.Threshold;
+import com.cv4j.core.datamodel.ByteProcessor;
+import com.cv4j.core.datamodel.CV4JImage;
+import com.cv4j.ocrdemo.camera.EasyCamera;
+import com.cv4j.ocrdemo.camera.util.DisplayUtils;
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button btnCapture;
+    private ImageView ivImage;
+    private ImageView ivImage2;
+    private TextView resultView;
+    private int screenWidth;
+    private float ratio = 0.5f; //取景框高宽比
+
+    private TessBaseAPI tessBaseApi;
+    private static final String lang = "eng";
+    private static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/TesseractSample/";
+    private static final String TESSDATA = "tessdata";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ivImage = (ImageView) findViewById(R.id.iv_image);
+        ivImage2 = (ImageView) findViewById(R.id.iv_image2);
+        btnCapture = (Button) findViewById(R.id.btn_capture);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String SAMPLE_CROPPED_IMAGE_NAME = "cropImage_"+System.currentTimeMillis()+".png";
+                Uri destination = Uri.fromFile(new File(getCacheDir(), SAMPLE_CROPPED_IMAGE_NAME));
+                EasyCamera.create(destination)
+                        .withViewRatio(ratio)
+                        .withMarginCameraEdge(50,50)
+                        .start(MainActivity.this);
+            }
+        });
+
+        screenWidth = (int) DisplayUtils.getScreenWidth(this);
+        ivImage.setLayoutParams(new LinearLayout.LayoutParams(screenWidth, (int) (screenWidth * ratio)));
+        ivImage.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        ivImage2.setLayoutParams(new LinearLayout.LayoutParams(screenWidth, (int) (screenWidth * ratio)));
+        ivImage2.setScaleType(ImageView.ScaleType.CENTER_CROP);
+
+        resultView = (TextView) findViewById(R.id.result);
+
+        prepareTesseract();
+    }
+
+    private void prepareTesseract() {
+        try {
+            prepareDirectory(DATA_PATH + TESSDATA);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        copyTessDataFiles(TESSDATA);
+    }
+
+    /**
+     * Prepare directory on external storage
+     *
+     * @param path
+     * @throws Exception
+     */
+    private void prepareDirectory(String path) {
+
+        File dir = new File(path);
+        if (!dir.exists()) {
+            if (!dir.mkdirs()) {
+                Log.e(TAG, "ERROR: Creation of directory " + path + " failed, check does Android Manifest have permission to write to external storage.");
+            }
+        } else {
+            Log.i(TAG, "Created directory " + path);
+        }
+    }
+
+    /**
+     * Copy tessdata files (located on assets/tessdata) to destination directory
+     *
+     * @param path - name of directory with .traineddata files
+     */
+    private void copyTessDataFiles(String path) {
+        try {
+            String fileList[] = getAssets().list(path);
+
+            for (String fileName : fileList) {
+
+                // open file within the assets folder
+                // if it is not already there copy it to the sdcard
+                String pathToDataFile = DATA_PATH + path + "/" + fileName;
+                if (!(new File(pathToDataFile)).exists()) {
+
+                    InputStream in = getAssets().open(path + "/" + fileName);
+
+                    OutputStream out = new FileOutputStream(pathToDataFile);
+
+                    // Transfer bytes from in to out
+                    byte[] buf = new byte[1024];
+                    int len;
+
+                    while ((len = in.read(buf)) > 0) {
+                        out.write(buf, 0, len);
+                    }
+                    in.close();
+                    out.close();
+
+                    Log.d(TAG, "Copied " + fileName + "to tessdata");
+                }
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "Unable to copy files to tessdata " + e.toString());
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == EasyCamera.REQUEST_CAPTURE) {
+                Uri resultUri = EasyCamera.getOutput(data);
+                int width = EasyCamera.getImageWidth(data);
+                int height = EasyCamera.getImageHeight(data);
+                ivImage.setImageURI(resultUri);
+
+                Log.i(TAG,"imageWidth:"+width);
+                Log.i(TAG,"imageHeight:"+height);
+
+                Observable.just(resultUri)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Consumer<Uri>() {
+                            @Override
+                            public void accept(Uri uri) throws Exception {
+                                startOCR(uri);
+                            }
+                        });
+
+            }
+        }
+    }
+
+    /**
+     * don't run this code in main thread - it stops UI thread. Create AsyncTask instead.
+     * http://developer.android.com/intl/ru/reference/android/os/AsyncTask.html
+     *
+     * @param imgUri
+     */
+    private void startOCR(Uri imgUri) {
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inSampleSize = 4; // 1 - means max size. 4 - means maxsize/4 size. Don't use value <4, because you need more memory in the heap to store your data.
+            Bitmap bitmap = BitmapFactory.decodeFile(imgUri.getPath(), options);
+
+            CV4JImage cv4JImage = new CV4JImage(bitmap);
+            Threshold threshold = new Threshold();
+            threshold.adaptiveThresh((ByteProcessor)(cv4JImage.convert2Gray().getProcessor()), Threshold.ADAPTIVE_C_MEANS_THRESH, 12, 30, Threshold.METHOD_THRESH_BINARY);
+            //threshold.process((ByteProcessor)(cv4JImage.convert2Gray().getProcessor()),Threshold.THRESH_OTSU,Threshold.METHOD_THRESH_BINARY,255);
+            Bitmap newBitmap = cv4JImage.getProcessor().getImage().toBitmap(Bitmap.Config.ARGB_8888);
+
+            ivImage2.setImageBitmap(newBitmap);
+
+            String result = extractText(newBitmap);
+            resultView.setText(result);
+
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+        }
+    }
+
+    private String extractText(Bitmap bitmap) {
+        try {
+            tessBaseApi = new TessBaseAPI();
+        } catch (Exception e) {
+            Log.e(TAG, e.getMessage());
+            if (tessBaseApi == null) {
+                Log.e(TAG, "TessBaseAPI is null. TessFactory not returning tess object.");
+            }
+        }
+
+        tessBaseApi.init(DATA_PATH, lang);
+
+//       //EXTRA SETTINGS
+//        //For example if we only want to detect numbers
+//        tessBaseApi.setVariable(TessBaseAPI.VAR_CHAR_WHITELIST, "1234567890");
+//
+//        //blackList Example
+//        tessBaseApi.setVariable(TessBaseAPI.VAR_CHAR_BLACKLIST, "!@#$%^&*()_+=-qwertyuiop[]}{POIU" +
+//                "YTRWQasdASDfghFGHjklJKLl;L:'\"\\|~`xcvXCVbnmBNM,./<>?");
+
+        Log.d(TAG, "Training file loaded");
+        tessBaseApi.setImage(bitmap);
+        String extractedText = "empty result";
+        try {
+            extractedText = tessBaseApi.getUTF8Text();
+        } catch (Exception e) {
+            Log.e(TAG, "Error in recognizing text.");
+        }
+        tessBaseApi.end();
+        return extractedText;
     }
 }
